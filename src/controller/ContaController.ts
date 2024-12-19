a22b8bf845355f249db9accbe6781936e5abde1c
Simone Gonzaga <simone.ggabriel@hotmail.com>
2024-12-18 21:04:21 -0300

    Adicionando Conta Controller

--- src/controller/ContaController.ts
+++ src/controller/ContaController.ts
@@ -1,66 +1,115 @@
-import { Conta } from "../model/Conta";
-import { ContaRepository } from "../repository/ContaRepository";
+import { Conta } from '../model/Conta'
+import { ContaRepository } from '../repository/ContaRepository'
 
-export class ContaController implements ContaRepository{
-    listarTodas(): void {
-        for (let conta of this.listaContas){
-            conta.visualizar()
-        }
-    }
-    sacar(numero: number, valor: number): void {
-        throw new Error("Method not implemented.");
-    }
-    depositar(numero: number, valor: number): void {
-        throw new Error("Method not implemented.");
-    }
-    transferir(numeroOrigem: number, numeroDestino: number, valor: number): void {
-        throw new Error("Method not implemented.");
-    }
+export class ContaController implements ContaRepository {
+	
+	// Coleção Array que vai armazenar os Objetos Conta
+	private listaContas = new Array<Conta>()
 
-// Colecao Array que vai armazenar os Objetos Conta
-private listaContas = new Array<Conta>();
+	// Controlar os Números das Contas
+	public numero: number = 0
 
-//Controlar os Numeros das Contas 
-public numero: number = 0; // public pq precisa acessar do menu
+	procurarPorTitular(titular: string): void {
+		
+		// Filtragem dos dados
+		let buscaPorTitular = this.listaContas.filter( conta => 
+			conta.titular.toUpperCase().includes(titular.toUpperCase())
+		)
 
-    procurarPorNumero(numero: number): void {
-        const buscaConta = this.buscarNoArray(numero);
+		// Listagem dos Dados
+		buscaPorTitular.forEach( conta => conta.visualizar() );
+	}
 
-        if (buscaConta !== null)
+	procurarPorNumero(numero: number): void {
+		const buscaConta = this.buscarNoArray(numero)
+
+		if (buscaConta !== null) 
             buscaConta.visualizar()
-        else
-            console.log("\n Conta não encontrada! ")    
-    }
+		else 
+            console.log('\nConta não Encontrada!')
+	
+     }
 
-    listaTodas(): void {
-        for (let conta of this.listaContas){
-            conta.visualizar
-        }
-    }
-    cadastrar(conta: Conta): void {
-        this.listaContas.push(conta)
-        console.log(" A conta foi cadastrada com sucesso! ")
-    }
-    atualizar(conta: Conta): void {
-        throw new Error("Method not implemented.");
-    }
-    deletar(numero: number): void {
-        throw new Error("Method not implemented.");
-    }
+	listarTodas(): void {
+		for (let conta of this.listaContas) {
+			conta.visualizar()
+		}
+	}
 
-    // Métodos Auxiliares 
+	public cadastrar(conta: Conta): void {
+		this.listaContas.push(conta)
+		console.log('A Conta foi cadastrada com sucesso!')
+	}
 
-    public gerarNumero(): number{
-    return ++this.numero;
-    }
+	atualizar(conta: Conta): void {
+		const buscaConta = this.buscarNoArray(conta.numero)
 
-    public buscarNoArray(numero: number): Conta | null{
-        for (let conta of this.listaContas){
-            if (conta.numero === numero)
-                return conta;
-        }
+		if (buscaConta !== null) {
+            this.listaContas[this.listaContas.indexOf(buscaConta)] = conta;
+			console.log("A Conta foi atualizada com sucesso!");
+		}else 
+            console.log('\nConta não Encontrada!')
+	}
 
-        return null;
-    }
-    
+	deletar(numero: number): void {
+		const buscaConta = this.buscarNoArray(numero)
+
+		if (buscaConta !== null) {
+            this.listaContas.splice(this.listaContas.indexOf(buscaConta), 1);
+			console.log("A Conta foi Deletada com sucesso!");
+		}else 
+            console.log('\nConta não Encontrada!')
+	}
+
+	// Métodos Bancários 
+	sacar(numero: number, valor: number): void {
+		const buscaConta = this.buscarNoArray(numero)
+
+		if (buscaConta !== null) {
+            if(buscaConta.sacar(valor) === true)
+				console.log("O Saque foi efetuado com sucesso!");
+		}else 
+            console.log('\nConta não Encontrada!')
+	}
+
+	depositar(numero: number, valor: number): void {
+		const buscaConta = this.buscarNoArray(numero)
+
+		if (buscaConta !== null) {
+            buscaConta.depositar(valor)
+			console.log("O Depósito foi efetuado com sucesso!");
+		}else 
+            console.log('\nConta não Encontrada!')
+	}
+
+	transferir(
+		numeroOrigem: number,
+		numeroDestino: number,
+		valor: number
+	): void {
+		const contaOrigem = this.buscarNoArray(numeroOrigem)
+		const contaDestino = this.buscarNoArray(numeroDestino)
+
+		if (contaOrigem !== null && contaDestino !== null) {
+            if(contaOrigem.sacar(valor) === true){
+				contaDestino.depositar(valor);
+				console.log("A Transferência foi efetuada com sucesso!");
+			}
+		}else 
+            console.log('\nConta de Origem e/ou Conta de Destonos não foi Encontrada!')
+	}
+
+	// Métodos Auxiliares
+
+	public gerarNumero(): number {
+		return ++this.numero
+	}
+
+	public buscarNoArray(numero: number): Conta | null {
+		for (let conta of this.listaContas) {
+			if (conta.numero === numero) return conta
+		}
+
+		return null
+	}
 }
